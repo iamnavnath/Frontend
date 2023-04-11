700704a77238fe95e1d30cb58fdb1ca0bccfd34e
Navnath Vasant Thorat <Navnath.Thorat@gisbizdev.gisbiz.chennai.com>
2023-04-11 19:10:39 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import { HomeComponent } from './home/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sidebar/sidebar/sidebar.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  @Input() isExpanded: boolean = false;
+  @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  handleSidebarToggle = () => this.toggleSidebar.emit(!this.isExpanded);
+
+}
